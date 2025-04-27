9b9e469a71256ba9a78a06e62d42f1858c2a75eb
KABOO33 <kamilbortnik7@gmail.com>
2025-04-27 20:10:04 +0200

    hi

---
+++ #1 - Timer/script.js
@@ -0,0 +1,53 @@
+const timer = document.getElementById('time');
+
+var time = 0;
+var inv = false;
+
+function newTime () {
+    time++;
+    refresh(time);
+}
+
+
+function refresh (time) {
+    let aaa= "";
+    let ttt = time;
+    let hours = String((ttt - ttt%360000)/360000);
+    ttt -= Number(hours) * 360000;
+    let minutes = String((ttt - ttt%6000)/6000);
+    ttt -= Number(minutes) *  6000;
+    let seconds = String((ttt - ttt%100)/100);
+    ttt -= Number(seconds) *  100;
+    let centoseconds = String(ttt);
+
+    if (Number(hours)>0){
+        aaa = hours + ":" + minutes + ":" + (Number(seconds)<10) ? "0" : none + seconds + "." + centoseconds;
+    } else {
+        aaa = minutes + ":" + seconds + "." + centoseconds;
+    }
+    console.log("Hours: " + hours)
+    console.log("Minutes: " + minutes)
+    console.log("Seconds: " + seconds)
+    console.log("Centoseconds: " + centoseconds)
+    timer.textContent = aaa;
+}
+
+function start () {
+
+    if (!inv){
+        inv = true;
+        invertal = setInterval(newTime, 10);
+    }
+}
+
+function stop () {
+    inv = false;
+    clearInterval(invertal);
+}
+
+function reset () {
+
+    stop ();
+    time = 0;
+    timer.textContent = "0:00.00";
+}
